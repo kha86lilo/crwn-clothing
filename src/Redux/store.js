91e04725fceb2a9a49d79074ae67b6419a35fa4e
Khalil Alodali <khalil.alodali@thegroup.com.qa>
2021-10-19 16:15:40 +0300

    removing middleware from prod build

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,7 +3,10 @@ import logger from 'redux-logger';
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist"; //[Study] add in order to persist the state in localstorage
 
-const applyMiddlewares = [logger];
+const applyMiddlewares =[];
+if (process.env.NODE_ENV === 'development') {
+    applyMiddlewares.push(logger);  
+}
 const store = createStore(rootReducer, applyMiddleware(...applyMiddlewares));
 const persistor = persistStore(store);
 
